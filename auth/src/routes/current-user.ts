7fa775b2f943269ea4be7764d233993763a9c3ed
MostafaAE <mostafa.aymanae@gmail.com>
2024-03-27 21:50:44 +0200

    Implement get current user endpoint

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentUser", (req, res, next) => {
-  res.send("Hi there!");
+router.get("/api/users/currentUser", currentUser, (req, res, next) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
