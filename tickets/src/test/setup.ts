0e9534723733f3c9e9b4144d04e467435d5ef884
MostafaAE <mostafa.aymanae@gmail.com>
2024-04-06 15:19:16 +0200

    Test create new ticket endpoint

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,10 +1,9 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import { app } from "../app";
-import request from "supertest";
+import jwt from "jsonwebtoken";
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -32,13 +31,22 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = "test@test.com";
-  const password = "password";
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({ email, password })
-    .expect(201);
-  const cookie = response.get("Set-Cookie");
-  return cookie!;
+global.signin = () => {
+  // Build a JWT Payload {id, email}
+  const payload = { id: "1mosta", email: "test@test.com" };
+
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session object
+  const session = { jwt: token };
+
+  // Turn session into json
+  const sessionJSON = JSON.stringify(session);
+
+  // Convert json to base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return a string thats the cookie with encoded data
+  return [`session=${base64}`];
 };
