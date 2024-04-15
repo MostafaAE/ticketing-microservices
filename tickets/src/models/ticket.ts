2b511e33c10f50c35b1e5f134833cc64ddf5be66
MostafaAE <mostafa.aymanae@gmail.com>
2024-04-15 16:50:50 +0200

    Include versions in ticket events

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 // An interface that describes the properties that are required to create a new ticket
 interface TicketAttrs {
@@ -17,6 +18,7 @@ interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
   userId: string;
+  version: number;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -38,12 +40,14 @@ const ticketSchema = new mongoose.Schema(
     toJSON: {
       transform(doc, ret) {
         ret.id = ret._id;
-        delete ret.__v;
         delete ret._id;
       },
     },
   }
 );
+
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
