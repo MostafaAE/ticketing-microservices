69eb9acfee57d1977a8c19c5f851d2fec215feae
MostafaAE <mostafa.aymanae@gmail.com>
2024-04-16 12:44:01 +0200

    Implement reject edits of reserved tickets in ticket update endpoint

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestError,
   NotAuthorizedError,
   NotFoundError,
   requireAuth,
@@ -27,6 +28,9 @@ router.put(
 
     const ticket = await Ticket.findById(req.params.id);
     if (!ticket) throw new NotFoundError();
+
+    if (ticket.orderId)
+      throw new BadRequestError("Cannot edit a reserved ticket");
 
     if (ticket.userId != req.currentUser!.id) throw new NotAuthorizedError();
 
